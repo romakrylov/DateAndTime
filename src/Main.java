806aa47508c1fa0b3c02aca41a9d2f20539ba951
Roman Krylov <0087897@gmail.com>
2022-08-07 17:01:22 +0300

    add new project timedate

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    DateTimeFormatter formatter =
+            DateTimeFormatter.ofPattern("HH:mm:ss MM/dd/yyyy");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now.format(formatter));
     }
 }
